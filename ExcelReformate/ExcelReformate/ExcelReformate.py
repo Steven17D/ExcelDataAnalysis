7b43233b843bc5ee8be21fc721827b9b7e8d78ab
Steven Dashevsky <stiven1999d@gmail.com>
2016-12-25 16:44:26 +0200

    New formate. Added style and custom save.

--- ExcelReformate/ExcelReformate/ExcelReformate.py
+++ ExcelReformate/ExcelReformate/ExcelReformate.py
@@ -4,24 +4,47 @@ import tkFileDialog
 from xlrd import open_workbook
 import xlwt
 
+def addStyle(path):
+    pass
+
 def saveFile(information, path = ''):
     path = path.split('.')
     path[-2] += '_NEW'
     path = '.'.join(path)
-
-    wbk = xlwt.Workbook() 
+    path = tkFileDialog.asksaveasfilename(initialfile = path)
+    wbk = xlwt.Workbook(style_compression=2) 
     sheet = wbk.add_sheet('Data')
-
-    for row, Rdata in enumerate(information):
+    for row, Rdata in enumerate(information[1:],1):
         for col, data in enumerate(Rdata):
-            sheet.write(row,col,data)
+            if (row - 1) % 18 == 0 :
+                if col == 3:
+                    sheet.write(row,col,data,style = xlwt.easyxf("borders: top double, left thin"))
+                else:
+                    sheet.write(row,col,data,style = xlwt.easyxf("borders: top double"))
+            elif col > 3 and (row - 1) % 3 == 0:
+                sheet.write(row,col,data,style = xlwt.easyxf("borders: top thin"))
+            elif col == 3 and (row - 1) % 3 == 0:
+                sheet.write(row,col,data,style = xlwt.easyxf("borders: top thin, left thin"))
+            elif col == 3:
+                sheet.write(row,col,data,style = xlwt.easyxf("borders: left thin"))
+            else:
+                sheet.write(row,col,data)
+    
+    for col, Fdata in enumerate(information[0]):
+        sheet.write(0,col,Fdata,style = xlwt.easyxf("font: bold on; borders: bottom thick"))
+    
+    for c in range(6):
+        sheet.col(c).width = 18*256
+
     wbk.save(path)
+
+    addStyle(path)
+
     os.startfile(path)
 
 def getDataFromFile(in_path):
     wb = open_workbook(in_path)
     for s in wb.sheets():
-        #print 'Sheet:',s.name
         values = []
         for row in range(s.nrows):
             col_value = []
@@ -38,13 +61,19 @@ def cleanUp(d = []):#, columnsToDelete = []):
 
 def reformate(data,title):
     newData = []
-    title = title[:10]
+    title = title[:6]
     title = map(lambda x: x.replace('_1','') ,title)
-    for row in data:
-        newData.append(row[:10]) #Main row + first body
-        for x in range(1,6):
-            offset = 3 + x*7
-            newData.append(['','',''] + row[offset:offset+7])
+    title = map(lambda x: x.replace('1','') ,title)
+    for rowNum, rowData in enumerate(data):
+        newData.append(rowData[:3] + rowData[3:6]) # append ==> [ProductType, RunName, Total_Scrap] + [ID,Scrap,Reason]
+        newData.append(['','','',''] + rowData[6:8])
+        newData.append(['','','',''] + rowData[8:10])
+        for x in range(1,6): # append ==> [ID,Scrap,Reason] * 5
+            idOff = 3 + x*7 # offset [ProductType, RunName, Total_Scrap] which is len 3 and every ID+Scraps len is 7
+            newData.append(['','',''] + rowData[idOff:idOff+3])
+            for y in range(1,3): # append ==> [Scrap,Reason] [Scrap,Reason]
+                scrapOff = idOff + y*3
+                newData.append(['','','',''] + rowData[scrapOff:scrapOff+2])
     newData.insert(0,title)
     return newData
 
@@ -55,7 +84,9 @@ def main():
     data = getDataFromFile(path)
     title = data[0]
     cleanUp(data)#, map(lambda x: title.index(x),['ID','Oprator','StartTime_Run','Shift']))
+    print "Working..."
     data = reformate(data[1:],title)
+    print "Saving..."
     saveFile(data,path)
     pass
     
---
+++ ExcelReformate/ExcelReformate/Py2EXE.bat
@@ -0,0 +1,1 @@
+pyinstaller -F -i ADAM.ico ExcelReformate.py
